e66e92b7ea5d5749eaf04c1128ce8824b789860f
wuxueming <1225929690@qq.com>
2018-05-15 14:18:25 +0800

    初次提交并没有写什么

---
+++ androidlibrary/src/main/java/com/example/androidlibrary/activity/BaseActivity.java
@@ -0,0 +1,37 @@
+package com.example.androidlibrary.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * @author wuxueming
+ * @date 2018/5/15
+ * @ToDo 规范化Activity
+ */
+
+public abstract class BaseActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        initVariables();
+        initLayout();
+        loadData();
+    }
+
+    /**
+     * 初始化变量，包括Intent带的数据和Activity内的变量
+     */
+    protected abstract void initVariables();
+
+    /**
+     * 加载layout布局文件
+     * @param savedInstanceState
+     */
+    protected abstract void initLayout();
+
+    /**
+     * 调用MobileAPI获取数据
+     */
+    protected abstract void loadData();
+}
--- app/src/main/java/com/example/xueming_wu/ant/MainActivity.java
+++ app/src/main/java/com/example/xueming_wu/ant/MainActivity.java
@@ -2,12 +2,56 @@ package com.example.xueming_wu.ant;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.androidlibrary.activity.BaseActivity;
+
+import org.w3c.dom.Node;
+
+import java.util.LinkedList;
+
+/**
+ *
+ */
+public class MainActivity extends BaseActivity {
+    private Manager manager=null;
+    private int m=0;
+
+    private static final String TAG = "MainActivity";
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    protected void initVariables() {
+        if(manager==null){
+            manager=new Manager();
+        }
+    }
+
+    @Override
+    protected void initLayout() {
         setContentView(R.layout.activity_main);
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                manager.doWork();
+
+
+            }
+        });
+    }
+
+    @Override
+    protected void loadData() {
+
+    }
+
+    private class  Manager{
+        public Manager(){
+            
+        }
+        public void doWork(){
+            m=++m;
+        }
     }
 }
